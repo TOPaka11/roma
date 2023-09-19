95dd08c1a4782a2589f8288d32c1e43fd03ce6ef
314 student <skola@gmail.com>
2023-09-19 21:41:28 +0200

    05

--- main.py
+++ main.py
@@ -3,17 +3,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#     print(3*3)
+#     input("Who are you?")
+#     print("What a life")
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(3*3)
-    input("Who are you?")
-    print("What a life")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def getCount(s):
+    return sum(c in 'aeiou' for c in s)
+print(getCount("akbkjuknknykkjekjkjbn"))

2df845304b47198e7e2b62c98250c15795a4b6d3
314 student <skola@gmail.com>
2023-09-21 19:40:52 +0200

    calculator

--- main.py
+++ main.py
@@ -15,19 +15,27 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def power(x, y):
+    return x ** y
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+def square_root(x):
+    return x ** 0.5
+
+
+print("Выберите операцию.")
+print("1.Сложить")
+print("2.Вычесть")
+print("3.Умножить")
+print("4.Разделить")
+print("5.Возвести в степень")
+print("6.Найти корень.")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5/6): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5', '6'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -46,3 +54,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == '5':
+            print(num1, "**", num2, "=", power(num1, num2))
+
+        elif choice == '6':
+            print(num1, "**", "0.5", "=", square_root(num1))
